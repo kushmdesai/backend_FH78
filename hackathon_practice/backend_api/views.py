d4158aea77ef3b15df27df3fb0c57294fde81658
kush <118694604+kushmdesai@users.noreply.github.com>
2025-04-13 08:35:35 -0400

    Update views.py fire hiding key

--- hackathon_practice/backend_api/views.py
+++ hackathon_practice/backend_api/views.py
@@ -6,7 +6,7 @@ from google import genai
 @csrf_exempt
 def generate_text(request):
     if request.method == "POST":
-        client = genai.Client(api_key="AIzaSyDsqllXdHi4MwFskfrIpaNOfMNOc4Yphhk")
+        client = genai.Client(api_key=process.env.GOOGLE_API_KEY)
         body = json.loads(request.body)
         input_text = body.get("input_text")
         result = client.models.generate_content(
