85d9ad612ab9cb7937a110f26e9807d61dd28b94
Kush <kushdesai221@gmail.com>
2025-04-14 18:32:32 -0400

    added max token counter

--- hackathon_practice/backend_api/views.py
+++ hackathon_practice/backend_api/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from google import genai
+from google.genai import types
 
 @csrf_exempt
 def generate_text(request):
@@ -12,6 +13,9 @@ def generate_text(request):
         result = client.models.generate_content(
             model="gemini-2.5-pro-exp-03-25",
             contents=[input_text],
+            config=types.GenerateContentConfig(
+            max_output_tokens=2000
             )
+        )
         return JsonResponse({"response": result.text})
     return JsonResponse({"error": "Only POST allowed"}, status=405)
